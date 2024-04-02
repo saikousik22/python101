db1f0863f505b9e723d61a7302f6e8906fb9cea3
saikousik22 <psaikousik@gmail.com>
2024-04-02 17:15:24 +0000

    data types

---
+++ 2--datatypes.py
@@ -0,0 +1,9 @@
+string = "Hello World "
+print(string.startswith("H")) # Boolean True
+print(string.endswith("d")) # Boolean True
+print(string.find("o")) # int 4
+print(string.index("o")) # int 4
+print(string.startswith("e")) # Boolean False
+print(string.endswith("H")) # Boolean False
+print(string)
+print(string.endswith(" ")) # Boolean True
