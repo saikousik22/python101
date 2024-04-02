c203162cf0d97707f956e5037e34837fba3ceb8e
saikousik22 <psaikousik@gmail.com>
2024-04-02 16:37:05 +0000

    my task 1

---
+++ 1--variables.py
@@ -0,0 +1,30 @@
+# this file explains variables in python
+
+print ('hello world')
+
+print ('12+31')
+
+# + addition
+# - Subtraction
+# /(float division) 
+# //(integer division)
+# * multiply
+# % modulo - remainder
+
+print(14//2)
+
+# type is a function to return the type of variable
+number = 1 #int
+print(type(number))
+
+number = float(number) #float
+print(type(number))
+
+number = int(number) #int
+print(type(number))
+#print(type(2.1))
+
+
+# find the syntax for floor and ceil equivalent functions in c
+
+
